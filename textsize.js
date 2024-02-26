1f76bac264a643e100550df3bc83b076f266a473
content-pages <161290566+content-pages@users.noreply.github.com>
2024-02-26 15:29:09 +0700

    Update textsize.js

--- textsize.js
+++ textsize.js
@@ -1,16 +1,18 @@
-$(function () {
-  $('a').click(function () {
-    var ourText = $('.post-body');
-    var currFontSize = ourText.css('fontSize');
+$(function(){
+    $(&#39;a&#39;).click(function(){
+    var ourText = $(&#39;.post-body&#39;);
+    var currFontSize = ourText.css(&#39;fontSize&#39;);
     var finalNum = parseFloat(currFontSize, 10);
     var stringEnding = currFontSize.slice(-2);
-    if (this.id == 'largesize') {
-      finalNum *= 1.1;
-    } else if (this.id == 'smallsize') {
-      finalNum /= 1.1;
-    } else if (this.id == 'normalsize') {
-      finalNum = 16;
+    if(this.id == &#39;largesize&#39;) {
+    finalNum *= 1.1;
     }
-    ourText.animate({ fontSize: finalNum + stringEnding }, 500);
-  });
+    else if (this.id == &#39;smallsize&#39;){
+    finalNum /=1.1;
+    }
+    else if (this.id == &#39;normalsize&#39;){
+    finalNum =16;
+    }
+    ourText.animate({fontSize: finalNum + stringEnding},500);
+    });
 });
